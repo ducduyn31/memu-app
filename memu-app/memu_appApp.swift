5ea2442a4a5459b9f4f2a60092a4f2ff1483b17f
ducduyn31 <ducduy.ababat@gmail.com>
2024-05-07 14:07:33 +1000

    Initial Commit

---
+++ memu-app/memu_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  memu_appApp.swift
+//  memu-app
+//
+//  Created by Duy Nguyen on 7/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct memu_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memu-appUITests/memu_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  memu_appUITestsLaunchTests.swift
+//  memu-appUITests
+//
+//  Created by Duy Nguyen on 7/5/2024.
+//
+
+import XCTest
+
+final class memu_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
