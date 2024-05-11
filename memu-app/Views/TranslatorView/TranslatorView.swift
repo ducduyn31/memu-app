d7301a095289504fb4267d807b934e076d23d6c2
kanethuong <kanethuong@gmail.com>
2024-05-11 16:57:47 +1000

    Add translator view

---
+++ memu-app/Views/TranslatorView/TranslatorView.swift
@@ -0,0 +1,18 @@
+//
+//  TranslatorView.swift
+//  memu-app
+//
+//  Created by Nguyễn Trần Trang Anh on 11/5/2024.
+//
+
+import SwiftUI
+
+struct TranslatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslatorView()
+}
