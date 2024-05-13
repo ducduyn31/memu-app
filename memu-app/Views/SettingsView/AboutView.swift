afa066822a4e92f146c8b422dd68a4a11f364905
kanethuong <kanethuong@gmail.com>
2024-05-13 12:59:15 +1000

    Add about view

--- memu-app/Views/SettingsView/AboutView.swift
+++ memu-app/Views/SettingsView/AboutView.swift
@@ -1,64 +1,18 @@
 //
 //  AboutView.swift
-//  MemuAI
+//  memu-app
 //
-//  Created by Kane Thuong on 13/05/2024.
+//  Created by Nguyễn Trần Trang Anh on 13/5/2024.
 //
 
 import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Spacer(minLength: CGFloat(100))
-            Text("Empowering Communication")
-                .font(.headline)
-            
-            Text("""
-                    MemuAI is dedicated to making everyday conversations inclusive for everyone. Our app translates sign language into text and speech in real-time, breaking down communication barriers and fostering understanding between the hearing and the deaf communities.
-                    """)
-            .font(.body)
-            
-            Spacer()
-            
-            Text("Key Features:")
-                .font(.headline)
-            
-            VStack(alignment: .leading, spacing: 10) {
-                FeatureView(icon: "video.fill", featureText: "Real-time sign language translation into text and speech.")
-                FeatureView(icon: "person.fill.viewfinder", featureText: "Live video processing for on-the-go interactions.")
-                FeatureView(icon: "message.fill", featureText: "Enables seamless communication with those who do not understand sign language.")
-            }
-            
-            Spacer()
-            
-            Text("Join us in bridging the gap in communication and embracing inclusivity with MemuAI.")
-                .font(.body)
-                .foregroundColor(.secondary)
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct FeatureView: View {
-    let icon: String
-    let featureText: String
-    
-    var body: some View {
-        HStack {
-            Image(systemName: icon)
-                .foregroundColor(.blue)
-                .frame(width: 30, height: 30)
-            Text(featureText)
-                .font(.body)
-        }
-    }
+#Preview {
+    AboutView()
 }
-
-struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
-    }
-}
-
-
