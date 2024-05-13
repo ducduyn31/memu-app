78fd024aaee7e4ee21c74e03497a31e24ea8886b
kanethuong <kanethuong@gmail.com>
2024-05-13 12:56:52 +1000

    Add about view

---
+++ memu-app/Views/SettingsView/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  memu-app
+//
+//  Created by Nguyễn Trần Trang Anh on 13/5/2024.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
